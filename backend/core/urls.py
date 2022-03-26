bda86ccedb823e72b0dfbf6bc1ce1837e205bc60
Alexander <alexsergeev45@gmail.com>
2022-03-26 16:32:56 +0300

    urls fix && start frontend part

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from albums.urls import AlbumRouter
-from artists.urls import ArtistRouter
-from genres.urls import GenreRouter
-from playlists.urls import PlaylistRouter
 
 
 urlpatterns = [
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import MainPage from './pages/MainPage/MainPage'
+import NotFound from './pages/NotFound/NotFound'
+
 
 function App() {
-
     return (
-        <div className="App">
-        </div>
+        <>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<MainPage />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+            <Footer />
+        </>
     )
 }
 
