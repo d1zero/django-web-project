2092d56e4122122256ac6d50827aff1e664a8088
Alexander <alexsergeev45@gmail.com>
2022-04-11 12:42:08 +0300

    last fix

--- core/settings.py
+++ core/settings.py
@@ -162,19 +162,14 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = "Europe/Moscow"
 
-sentry_sdk.init(
-    dsn=env('SENTRY_DSN'),
-    integrations=[DjangoIntegration()],
+# sentry_sdk.init(
+#     dsn=env('SENTRY_DSN'),
+#     integrations=[DjangoIntegration()],
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+#     traces_sample_rate=1.0,
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+#     send_default_pii=True
+# )
 
 BATON = {
     'SITE_HEADER': 'Musicality',
